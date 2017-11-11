d6a542d9390ab83932b286088538d4582c6acf83
Jackson Delahunt <jackson@stemn.com>
2017-11-11 10:20:34 +1000

    add global error handler

--- index.js
+++ index.js
@@ -14,41 +14,54 @@ const emptyBucket = async (Bucket, options) => {
     }
   }
 
-  const deleteObjects = async (objects) => {
-    // before we can delete the bucket, we must delete all versions of all objects
-    const Objects = objects.map(({ Key, VersionId }) => ({ Key, VersionId }));
+  const emptyBucket = async () => {
+    const deleteObjects = async (objects) => {
+      // before we can delete the bucket, we must delete all versions of all objects
+      const Objects = objects.map(({ Key, VersionId }) => ({ Key, VersionId }));
 
-    await s3.deleteObjects({
-      Bucket,
-      Delete: { Objects }
-    }).promise()
+      await s3.deleteObjects({
+        Bucket,
+        Delete: { Objects }
+      }).promise()
+    };
+
+    // get the list of all objects in the bucket
+    const { Versions } = await s3.listObjectVersions({ Bucket }).promise();
+
+    // if the bucket contains objects, delete them
+    debug(`Deleting ${Versions.length} object versions`);
+    if (Versions.length > 0) {
+      await deleteObjects(Versions);
+    }
+
+    // check for any files marked as deleted previously
+    const { DeleteMarkers } = await s3.listObjectVersions({ Bucket }).promise();
+
+    // if the bucket contains delete markers, delete them
+    debug(`Deleting ${DeleteMarkers.length} object delete markers`);
+    if (DeleteMarkers.length > 0) {
+      await deleteObjects(DeleteMarkers);
+    }
+
+    // if there are any non-versioned contents, delete them too
+    const { Contents } = await s3.listObjectsV2({ Bucket }).promise();
+
+    // if the bucket contains delete markers, delete them
+    debug(`Deleting ${Contents.length} objects`);
+    if (Contents.length > 0) {
+      await deleteObjects(Contents);
+    }
   };
 
-  // get the list of all objects in the bucket
-  const { Versions } = await s3.listObjectVersions({ Bucket }).promise();
-
-  // if the bucket contains objects, delete them
-  debug(`Deleting ${Versions.length} object versions`);
-  if (Versions.length > 0) {
-    await deleteObjects(Versions);
-  }
-
-  // check for any files marked as deleted previously
-  const { DeleteMarkers } = await s3.listObjectVersions({ Bucket }).promise();
-
-  // if the bucket contains delete markers, delete them
-  debug(`Deleting ${DeleteMarkers.length} object delete markers`);
-  if (DeleteMarkers.length > 0) {
-    await deleteObjects(DeleteMarkers);
-  }
-
-  // if there are any non-versioned contents, delete them too
-  const { Contents } = await s3.listObjectsV2({ Bucket }).promise();
-
-  // if the bucket contains delete markers, delete them
-  debug(`Deleting ${Contents.length} objects`);
-  if (Contents.length > 0) {
-    await deleteObjects(Contents);
+  try {
+    debug(`Emptying bucket`);
+    await emptyBucket();
+  } catch (e) {
+    if (e.code === 'NoSuchBucket') {
+      debug('Bucket not found!');
+    } else {
+      throw e;
+    }
   }
 };
 
